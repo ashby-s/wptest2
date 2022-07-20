34e2c49287e62d0b687c816e1768a9d2d9618457
ashby-s <109519556+ashby-s@users.noreply.github.com>
2022-07-20 13:35:46 +0100

    Add Alert Heading

--- src/common/pages/index.tsx
+++ src/common/pages/index.tsx
@@ -2,6 +2,7 @@ import { FC, Fragment, createElement as h } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { PageProps } from '@not-govuk/app-composer';
 import config from '../config';
+import {Alert} from '@hods/components';
 
 const siteTitle = config.title;
 
@@ -10,6 +11,23 @@ const description = 'Our homepage';
 
 const Page: FC<PageProps> = props => (
   <Fragment>
+  	<Alert heading="Test passport">
+  	We'll send your new passport by secure delivery. The
+  	cost is included in the passport fee.
+	</Alert>
+    	<Alert
+  	heading="Payment successful"
+  	classModifiers="success">
+  	We've sent details to test-email@test-corp.co.uk{" "}
+  	<br />
+  	<a href="#">Download confirmation</a>
+	</Alert>
+	<Alert
+  	heading="Payment unsuccessful"
+  	classModifiers="error">
+  	Please try again or contact our{" "}
+  	<a href="#">support team</a> for assistance.
+	</Alert>
     <Helmet>
       <title>{title} - {siteTitle}</title>
       <meta name="description" content={description} />
